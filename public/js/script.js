ea081ef7fe30dd038fc5fc378d58a09b27b324d9
Reda <ahmedreda5338@gmail.com>
2024-05-06 20:21:09 +0300

    edit answers

--- public/js/script.js
+++ public/js/script.js
@@ -3,9 +3,9 @@ const questions = {
         { id: 1, q: "What should I do if my smart ID card is lost or stolen?", a: "If your smart ID card is lost or stolen, any attempt to access it within the system will be unauthorized until a report is made. For assistance, please contact us immediately at 011 822 0000." },
         { id: 2, q: "How do I update the personal information associated with my smart ID card?", a: "If you want to update your personal information, please head to the Student Affairs Administration." },
         { id: 3, q: "What should I do if my smart ID card is not working properly?", a: "Please call (011 822 0000) to repair your card or to be provided with a temporary permit." },
-        { id: 4, q: "How do I verify my attendance using the smart ID card?", a: "Control panel." },
-        { id: 5, q: "Is there available parking?", a: "Control panel." },
-        { id: 6, q: "When will the next metro arrive?", a: "Control panel." },
+        { id: 4, q: "How do I verify my attendance using the smart ID card?", a: "<img src=\"img/attend1.jpeg\" alt=\"\" class=\"mb-4\"><img src=\"img/attend2.jpeg\" alt=\"\">" },
+        { id: 5, q: "Is there available parking?", a: "<img src=\"img/parking.jpeg\" alt=\"\">" },
+        { id: 6, q: "When will the next metro arrive?", a: "<img src=\"img/metro.jpeg\" alt=\"\">" },
         { id: 7, q: "How can I borrow a book from the library?", a: "To borrow a book, you must register for the requested book and then scan your card to complete the registration." },
         { id: 8, q: "How can a visitor obtain permission to access university facilities?", a: "To obtain a visitor's permit to access Princess Nourah University, please contact Tawasul Nourah through the provided link. <a href=\"https://tawasulnourah.pnu.edu.sa/\" target=\"_blank\" class=\"text-blue-500 hover:text-blue-700 underline\">tawasulnourah.pnu.edu.sa/</a>" },
         { id: 9, q: "Other Inquiries", a: "For other inquiries or questions, please refer to the following email/phone number: <a href=\"https://Info@pnu.edu.sa\" target=\"_blank\" class=\"text-blue-500 hover:text-blue-700 underline\">Info@pnu.edu.sa</a>  (+966118220000)" }
@@ -15,8 +15,8 @@ const questions = {
         { id: 2, q: "كيف أقوم بتحديث المعلومات الشخصية المرتبطة ببطاقة الهوية الذكية؟", a: "إذا أردت تحديث معلوماتك الشخصية، يرجى التوجه الى إدارة شؤون الطالبات." },
         { id: 3, q: "ماذا يجب أن أفعل إذا لم تعمل بطاقة الهوية الذكية بشكل صحيح؟", a: "يرجى الاتصال (011 822 0000) لإصلاح بطاقتك أو لتزويدك بتصريح مؤقت." },
         { id: 4, q: "كيف أتحقق من حضوري باستخدام بطاقة الهوية الذكية؟", a: "لوحة التحكم." },
-        { id: 5, q: "هل يوجد مكان لوقوف السيارات متاح؟", a: "لوحة التحكم." },
-        { id: 6, q: "متى ستصل أقرب مترو؟", a: "لوحة التحكم." },
+        { id: 5, q: "هل يوجد مكان لوقوف السيارات متاح؟", a: "<img src=\"img/parking.jpeg\" alt=\"\">" },
+        { id: 6, q: "متى ستصل أقرب مترو؟", a: "<img src=\"img/metro.jpeg\" alt=\"\">" },
         { id: 7, q: "كيف يمكنني استعارة كتاب من المكتبة؟", a: "لاستعارة كتاب، يجب عليك التسجيل للكتاب المطلوب ثم مسح بطاقتك لإكمال التسجيل." },
         { id: 8, q: "كيف يمكن للزائر الحصول على إذن للوصول إلى مرافق الجامعة؟", a: "للحصول على تصريح زائر للوصول إلى جامعة الأميرة نورة، يرجى الاتصال بتواصل نورة من خلال الرابط المقدم. <a href=\"https://tawasulnourah.pnu.edu.sa/\" target=\"_blank\" class=\"text-blue-500 hover:text-blue-700 underline\">tawasulnourah.pnu.edu.sa/</a>" },
         { id: 9, q: "أخرى", a: "إذا كان لديك أي استفسارات أو أسئلة أخرى الرجوع إلى البريد الإلكتروني/الرقم التالي: <a href=\"https://Info@pnu.edu.sa\" target=\"_blank\" class=\"text-blue-500 hover:text-blue-700 underline\">Info@pnu.edu.sa</a>   (+966118220000)"}
@@ -91,7 +91,20 @@ function scrollToBottom() {
     var messagesContainer = document.getElementById('messages-container');
     var bottomElement = messagesContainer.lastElementChild;
     if (bottomElement) {
-        bottomElement.scrollIntoView({ behavior: "smooth", block: "end" });
+        var images = bottomElement.getElementsByTagName('img');
+        if (images.length > 0) {
+            var loadedImages = 0;
+            for (var i = 0; i < images.length; i++) {
+                images[i].onload = () => {
+                    loadedImages++;
+                    if (loadedImages === images.length) {
+                        bottomElement.scrollIntoView({ behavior: "smooth", block: "end" });
+                    }
+                };
+            }
+        } else {
+            bottomElement.scrollIntoView({ behavior: "smooth", block: "end" });
+        }
     }
 }
 
